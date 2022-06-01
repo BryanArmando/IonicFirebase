501fce775a2638c501aaccd0d42721ee666b45e4
BryanArmando <bryanquisaguano@gmail.com>
2022-06-01 01:45:10 -0500

    logearse con firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'ionic-2022-7ae74',
+    appId: '1:519169093359:web:872674c9884426d328b51a',
+    storageBucket: 'ionic-2022-7ae74.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBgRsM1wibR_5U2aRDKoOn-shCUKsZg0oQ',
+    authDomain: 'ionic-2022-7ae74.firebaseapp.com',
+    messagingSenderId: '519169093359',
+    measurementId: 'G-HG0L1ZSBB7',
+  },
   production: true
 };
